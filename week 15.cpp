71d0a57f2d2df2044cf58c114b97fade67e295ae
Stanislav Gandzhov <152076566+Stanismad1@users.noreply.github.com>
2024-02-24 20:15:55 +0200

    Add files via upload

---
+++ week 15.cpp
@@ -0,0 +1,110 @@
+using namespace std;
+#include <iostream>
+
+int main()
+{
+#include <iostream>
+#include <cmath>
+
+    class Point
+    {
+    public:
+
+        double x, y;
+
+        double distanceFromOrigin()
+        {
+            return sqrt(x * x + y * y);
+        }
+
+        void translate(double vx, double vy)
+        {
+            this->x += vx;
+            /*this->*/y += vy;
+        }
+    };
+  
+    class Vector2D
+    {
+    public:
+        double a, b;
+
+        Vector2D plus(Vector2D other)
+        {
+            Vector2D r = { this->a + other.a,this->b + other.b };
+    
+            return r;
+        }
+
+        Vector2D operator + (Vector2D other)
+        {
+            return this->plus(other);
+        }
+
+        void print()
+        {
+            std::cout << "(" << a << "," << b << ")\n";
+        }
+
+        Vector2D operator * (double a)
+        {
+            Vector2D r = { this->a * a, b * a };
+            return r;
+        }
+
+        double operator * (Vector2D other)
+        {
+            return a * other.a + b * other.b;
+        }
+
+        Vector2D operator *= (double a)
+        {
+            this->a *= a;
+            b *= a;
+
+            return *this;
+        }
+
+    };
+
+    int main()
+    {
+        Point p1 = { 0,0 };
+        Point p2 = { 1,1 };
+
+        std::cout << "Dist orig p1 = " << p1.distanceFromOrigin() << std::endl;
+        std::cout << "Dist orig p2 = " << p2.distanceFromOrigin() << std::endl;
+
+        p1.translate(2, 0);
+
+        std::cout << "Dist orig p1 = " << p1.distanceFromOrigin() << std::endl;
+        std::cout << "Dist orig p2 = " << p2.distanceFromOrigin() << std::endl;
+
+        Vector2D v1 = { 0,1 };
+        Vector2D v2 = { 2,3 };
+
+        v1.print();
+        v2.print();
+
+        v1.plus(v2).print();
+        
+        Vector2D v3 = v1 + v2;
+       
+        v3.print();
+
+        v3 = v3 * 2;
+        v3.print();
+
+        Vector2D v4 = v3 *= 2;
+        v4.print();
+
+        std::cout << "v1*v2 = " << v1 * v2 << std::endl;
+
+        Vector2D vectors[] = { v1,v2,v3 };
+
+        (vectors[2] + vectors[0] * (vectors[1] * v4)).print();
+
+        return 0;
+    }
+
+
